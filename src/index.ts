0ec22caf6610a35790c11b4a5d9e65c54640acf1
Jakub Kaczmarzyk <jakubk@mit.edu>
2019-03-26 09:24:24 -0400

    use ungzip instead of inflate

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,12 @@ export * from './types';
 export * from './header';
 export * from './image';
 
-import {inflate, isGzipped} from './gzip';
+import {ungzip, isGzipped} from './gzip';
 import {Image} from './image';
 
 export function fromBuffer(buffer: ArrayBuffer): Image {
   if (isGzipped(buffer)) {
-    buffer = inflate(buffer);
+    buffer = ungzip(buffer);
   }
   const image = Image.fromBuffer(buffer);
   return image;
